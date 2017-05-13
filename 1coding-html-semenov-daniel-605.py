108de3ae22fca19b9c82f7e1c6e7c8c8c581ebe4
Daniel <Daniel>
2017-05-13 17:57:39 +0300

    commit

---
+++ 1coding-html-semenov-daniel-605.py
@@ -0,0 +1,12 @@
+f = open('semenovadaniel-11-605.html','w')
+
+message = """<html>
+<head><title>Семенов Даниэль 11-605</title></head>
+<body>
+<p>HTML файл содержащий таблицу умножения © Семенов Даниэль 11-605 </p>
+<img src="http://formula-xyz.ru/images/algebra/tablitsyi/tablitsa-umnozheniya-1-10.jpg" alt="tablica umnojeniya" width="652" height="494">
+</body>
+</html>"""
+
+f.write(message)
+f.close()
